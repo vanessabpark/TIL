376e2c8a366b7c1eb9b63b3d400b125a82dcdbea
Vanessa Park <bohyunparkv@gmail.com>
2023-02-03 20:10:08 +0900

    Implementing a different method to the questions: solving it for the second time

---
+++ leet-code/easy/RomanToInteger.js
@@ -0,0 +1,34 @@
+## 13. Roman to Integer
+
+var romanToInt = function(s) {
+    let symbols = {
+        "I" : 1,
+        "V": 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
+    
+    let sum = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let curr = symbols[s.charAt(i)];
+        let next = symbols[s.charAt(i+1)];
+
+    if (next) {
+            if (curr >= next) {
+                sum += curr;
+            } else {
+                sum += (next - curr);
+                i++;
+            }
+    } else {
+           sum += curr;
+       }
+    } return sum;
+}
+
+### REMEMBER: find a pattern 
+### ex) a letter before and after
