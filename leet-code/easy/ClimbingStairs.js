bb1412835377bd7daf4b427b51f8a7187a594ae5
Vanessa Park <bohyunparkv@gmail.com>
2023-02-22 23:31:39 +0900

    Attempting to solve a Climbing Stairs question

---
+++ leet-code/easy/ClimbingStairs.js
@@ -0,0 +1,10 @@
+var climbStairs = function (n) {
+  let countingFunc = function (stairsRemaining, savedResults) {
+    if (stairsRemaining < 0) {
+      return 0;
+    }
+    if (stairsRemaining === 0) {
+      return 1;
+    }
+  };
+};
