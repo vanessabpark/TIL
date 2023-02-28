e94096db508a7760c5f14c63d5907dd77cc50903
Vanessa Park <bohyunparkv@gmail.com>
2023-02-28 09:28:39 +0900

    Merging two sorted arrays using two pointers

---
+++ leet-code/easy/MergeSortedArray.js
@@ -0,0 +1,17 @@
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (second < 0) {
+      break;
+    }
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
+};
