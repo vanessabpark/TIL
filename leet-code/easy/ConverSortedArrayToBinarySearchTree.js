a39cd4de8edcb53480a56eda1b908f89778f721b
Vanessa Park <bohyunparkv@gmail.com>
2023-07-17 09:25:24 +0100

    Binary search tree question using recursive method

---
+++ leet-code/easy/ConverSortedArrayToBinarySearchTree.js
@@ -0,0 +1,6 @@
+// hard one
+// need an understanding of height-balanced binary search tree
+
+// Height Balanced Binary Tree: a binary tree in which the height of the left subtree and right subtree of any node does not differ by more than 1 and both the left and right subtree are also height balanced.
+
+// need to examine the solution more
